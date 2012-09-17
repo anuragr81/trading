06c20ce56c14dcf2814221da4d587e784e39c315
anuragr81 <anandmukh@yahoo.com>
2012-09-17 19:17:11 +0200

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -14,20 +14,20 @@ class Portfolio:
      self.tolerance = tol
 
   def trade(self,ticker,qty): # +;- :: buy;sell
-     if ticker in self.tickers.keys() : 
+     if ticker in self.tickers.keys() :
        self.tickers[ticker]=self.tickers[ticker]+qty
 
+  def evaluate_instr(self,ticker,time,wopdict):
+     wop = wopdict[ticker]
+     closest_time = wop.closest_time(time);
+     if time - closest_time > self.tolerance :
+          raise PriceNotFoundException();
+     else :
+          return float(wop[closest_time][0])
 
-  def evaluate(self,time,wopdict): 
+  def evaluate(self,time,wopdict):
       sum = 0
-      for tkr in self.tickers.keys(): 
-         #print "time=",time
-	 wop = wopdict[tkr]
-         closest_time = wop.closest_time(time);
-	 if time - closest_time > self.tolerance : 
-	   raise PriceNotFoundException();
-	 else : 
-           price = float(wop[closest_time][0])
-	   #print "Time =", closest_time, " Price=",price
-           sum = sum + price*self.tickers[tkr]
+      for tkr in self.tickers.keys():
+          sum = sum + self.evaluate_instr(tkr,time,wopdict)*self.tickers[tkr]
+
       return sum
