0d5517de696d120e61061e9b210225c55d535401
anurag <anandmukh@yahoo.com@git config --global user.name anuraggit config --global user.email anandmukh@yahoo.com@>
2015-05-16 19:55:37 +0000

    Adding today's date.

--- getdata.sh
+++ getdata.sh
@@ -4,14 +4,14 @@ start_day=26
 start_month=02
 start_year=1990
 
-end_day=10
-end_month=01
-end_year=2012
+end_day=`date +'%d'`
+end_month=`date +'%m'`
+end_year=`date +'%Y'`
 
 cat tickers.lst | while read ticker ;
 do
   url="http://ichart.finance.yahoo.com/table.csv?s=$ticker&a=$start_month&b=$start_day&c=$start_year&d=$end_month&e=$end_day&f=$end_year&g=d&ignore=.csv"
-  ./fetch.sh $url data/$ticker.html
+  bash fetch.sh $url data/$ticker.html
   echo $url
 done
 
