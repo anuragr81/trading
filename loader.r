12c200107e8f195f5c51be44c7226f07be4236ac
anurag <anandmukh@yahoo.com@git config --global user.name anuraggit config --global user.email anandmukh@yahoo.com@>
2015-05-28 10:30:39 +0000

    Adding code to write results.

--- loader.r
+++ loader.r
@@ -34,9 +34,21 @@ myrollapply <- function (dat,width,func){
    if( rowlen < width ) {
       stop('dim(dat)[1] < width');
    }
+   returns_array = NULL;
+   factor_array = NULL;
+   date_array = NULL;
    for ( i in seq(1,rowlen-width+1)) {
-       print (returnVec(dat[i:(i+width-1),col]));
+       date_data = dat[i:(i+width-1),"Date"];
+       col_data = dat[i:(i+width-1),col];
+       #print (paste("(",head(date_data,1),"-",tail(date_data,1),") : ",returnVec(col_data),head(col_data,1),1e+5*returnVec(col_data)/head(col_data,1)));
+       date_array=c(date_array,strftime(head(date_data,1),"%Y-%m-%d"));
+       returns_array=c(returns_array,returnVec(col_data));
+       factor_array=c(factor_array,1e+5*returnVec(col_data)/head(col_data,1));
    }
+   output_df = data.frame(Date=date_array,ret=returns_array,real_factor=factor_array);
+
+   write.csv(output_df,"/tmp/output.df.csv");
+   return(output_df);
 }
 # simple and fast returns plot (for 10x5 blocks) would be:
 # dat5 = dataNdaySpaced(loader("data/NAME.csv"),5);
