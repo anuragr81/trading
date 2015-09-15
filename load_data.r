b1dc23936df9f41189093ffb71a7727c3ea7a33b
anurag <anandmukh@yahoo.com@git config --global user.name anuraggit config --global user.email anandmukh@yahoo.com@>
2015-09-15 06:18:37 +0000

    Adding functionality to retrieve data until a certain point.

--- load_data.r
+++ load_data.r
@@ -1,6 +1,15 @@
 
 #dat=read.csv('data/YHOO.html');
 dat=read.csv(commandArgs(TRUE)[1]);
+run_date=strptime(commandArgs(TRUE)[2],"%Y-%m-%d");
 tx = strptime(dat$Date,"%Y-%m-%d");
-plot(tx,dat$Open,'l');
+dat$date<-tx
+plot(dat$date,dat$Open,'l');
+if (!is.na(run_date)){
+ dat = dat[dat$date<=run_date,]
+ print (dat[1:2,])
+
+} else {
+stop(paste("RunDate:",run_date)) 
+}
 
