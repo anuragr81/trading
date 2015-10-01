05d611f02c890f40569939c3e780f6da4b419e85
Your Name <you@example.com>
2015-10-01 15:02:49 +0100

    Calculating running positions.

--- pnl_vector.r
+++ pnl_vector.r
@@ -1,3 +1,12 @@
+num_to_transact <- function(position,position_limit,price){
+  num = (position_limit - position)/price;
+  if (abs(num)>=1){
+    return(num);
+  } else{
+    return(0);
+  }
+  
+}
 
 get_pnl_vector <- function(date_vector,price_vector,signals,start_position,position_limit){
   
@@ -14,23 +23,30 @@ get_pnl_vector <- function(date_vector,price_vector,signals,start_position,posit
     stop("date_vector and signals must be of the same size");
   }
   pnl=array();
-  for (i in seq(len(date_vector))){
+  position =0;
+  for (i in seq(length(date_vector))){
     signal <- signals[i];
     if (signal == 1){
       # buy
+      n = num_to_transact(position=position,position_limit=position_limit,price=price_vector[i]);
+      position = position + price_vector[i]*n;
+      print(paste("position=",position));
     } else if (signal == -1)
     {
       # sell
+      n = num_to_transact(position=position,position_limit=0,price=price_vector[i]);
+      position = position + price_vector[i]*n;
+      print(paste("position=",position));
     }
     else if (signal ==0){
       # do nothing
     } else{
       stop("Error in signal");
     }
+    pnl[i]=0;
   }
-  pnl[i]=0;
-}
-return(0);
+  
+  return(0);
 }
 
 
