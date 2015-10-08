6d0bcbd87e81e6dbfcd68920aa8a07469e78786f
Your Name <you@example.com>
2015-10-08 15:45:14 +0100

    Adding vol.

--- signal_vector.r
+++ signal_vector.r
@@ -1,8 +1,59 @@
 
 source('utils.r');
+
 # @desc          - returns signal using ALL data provided
 # @preconditions - assumes date_vector and price_vector are of same length
-get_signal_at_last_point <- function(date_vector,price_vector,tol) {
+get_vol_signal_at_last_point <- function(date_vector,price_vector,tol) {
+  window_size <- 5
+  ma_size <- 5
+  if(ma_size>window_size){
+    stop("must have ma_size <= window_size")
+  }
+  n <- length(price_vector);
+  if (n>window_size) {
+    print(paste("Using data from:",date_vector[1]," to ",date_vector[n]))
+    print(paste("last_price=",price_vector[n]));
+    diff_price_vector = diff(price_vector)
+    sz_diff_price_vector = length(diff_price_vector)
+    last_diffs = (diff_price_vector[(sz_diff_price_vector-2):sz_diff_price_vector])
+    signs = (sign(last_diffs))
+    print(paste("signs=",toString(signs)))
+    
+    if (sum(signs-c(-1,-1,-1))==0){
+      return(1);
+    }
+    if (sum(signs!=c(-1,-1,1))==0){
+      return(1);
+    }
+    if (sum(signs-c(-1,1,-1))==0){
+      return(-1);
+    }
+    if (sum(signs-c(-1,1,1))==0){
+      return(1);
+    }
+    if (sum(signs-c(1,-1,-1))==0){
+      return(-1);
+    }
+    if (sum(signs-c(1,-1,1))==0){
+      return(1);
+    }
+    if (sum(signs-c(1,1,-1))==0){
+      return(-1);
+    }
+    if (sum(signs-c(1,1,1))==0){
+      return(1);
+    }
+    
+    #stop("DONE")
+    # if condition is met (if price hits below mean-average then buy)
+    
+  }
+  return(0);
+}
+
+# @desc          - returns signal using ALL data provided
+# @preconditions - assumes date_vector and price_vector are of same length
+get_ma_signal_at_last_point <- function(date_vector,price_vector,tol) {
   window_size <- 5
   ma_size <- 5
   if(ma_size>window_size){
@@ -13,16 +64,6 @@ get_signal_at_last_point <- function(date_vector,price_vector,tol) {
     print(paste("Using data from:",date_vector[1]," to ",date_vector[n]))
     print(paste("moving average=",mean(price_vector[n-ma_size:n])));
     print(paste("last_price=",price_vector[n]));
-    
-    # temporary stop condition <start>
-    #if(n==20){
-    #  dat<-data.frame(date=date_vector,price=price_vector);
-    #  print(dat);
-    #  plot(dat$date,dat$price);
-    #  stop("Done");
-    #}
-    
-    # temporary stop condition <end>
     
     # if condition is met (if price hits below mean-average then buy)
     if (price_vector[n]<mean(price_vector[(n-ma_size):n]*(1-tol) )) {
@@ -57,7 +98,7 @@ get_signal_vector <- function(date_vector,price_vector) {
   tol <- .005
   for ( i in seq(length(date_vector))) {
     # use different functions for different signal calculation criteria
-    signals[i] = get_signal_at_last_point(date_vector=date_vector[1:(i-1)],price_vector=price_vector[1:(i-1)],tol=tol);
+    signals[i] = get_vol_signal_at_last_point(date_vector=date_vector[1:(i-1)],price_vector=price_vector[1:(i-1)],tol=tol);
     print(paste("signal for ",date_vector[i], " was ", signals[i], " price today: ",price_vector[i]  ));
   }
   
