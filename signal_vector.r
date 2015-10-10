d21fc83d19f8318cdf09373eddd49e3cd2e7ca1d
Your Name <you@example.com>
2015-10-10 16:17:01 +0100

    Plotting moving sd.

--- signal_vector.r
+++ signal_vector.r
@@ -13,24 +13,17 @@ get_vol_signal_at_last_point <- function(date_vector,price_vector,tol) {
   
   status=list();
   status$signal=0;
-  status$vector=c(0,0,0);
-  
+  status$vector=c(0)
   if (n>window_size) {
+    
     print(paste("Using data from:",date_vector[1]," to ",date_vector[n]))
     print(paste("last_price=",price_vector[n]));
     diff_price_vector = diff(price_vector)
     sz_diff_price_vector = length(diff_price_vector)
-    last_diffs = (diff_price_vector[(sz_diff_price_vector-2):sz_diff_price_vector])
-    signs = (sign(last_diffs))
-    print(paste("price=",toString(price_vector[(n-3):n])))
-    print(paste("signs=",toString(signs)))
-    status$vector=signs;  
-    
+    status$vector = sd(diff_price_vector[(sz_diff_price_vector-window_size):sz_diff_price_vector]);
   }
-  
   return(status);
-  
-  return(0);
+  #return(0);
 }
 
 # @desc          - returns signal using ALL data provided
@@ -63,6 +56,26 @@ get_ma_signal_at_last_point <- function(date_vector,price_vector,tol) {
   return(0);
 }
 
+plot_signal_vectors<-function (test_results)
+{
+  # plotting test_results <start>
+  len_signals = length(names(test_results));
+  #             toString(summary(res)$coef[,"t value"][2]),
+  print(paste("total number of signs: ",len_signals))
+  par(mfrow=c(4,5))
+  for (i in seq(len_signals)){
+    
+    sd_val = sd(test_results[[i]]);
+    if (!is.na(sd_val) && sd_val<1 && length(test_results[[i]]) > 10){
+      plot(test_results[[i]],xlab=
+             paste(names(test_results)[i],"(sd=",sd_val,")")
+      )
+      print(paste(names(test_results)[i],"(sd=",sd_val,")"))   
+    }
+  }
+  # plotting test_results <end>
+}
+
 #if ( numToBuy(price=price_vector[n],position=position,poisition_limit=position_limit)
 # @desc - returns signal for all input vectors
 get_signal_vector <- function(date_vector,price_vector) {
@@ -78,37 +91,27 @@ get_signal_vector <- function(date_vector,price_vector) {
   }
   signals = array();
   tol <- .005
-  test_results=list();
+  test_results=array();
+  observed_jump=array();
   for ( i in seq(length(date_vector))) {
     # use different functions for different signal calculation criteria
     
     status = get_vol_signal_at_last_point(date_vector=date_vector[1:(i-1)],price_vector=price_vector[1:(i-1)],tol=tol);
-    signals[i] =status$signal;
-    ## populating test_results<start>
-    if (length(names(test_results)[names(test_results)==toString(status$vector)])==0){
-      #print(paste(toString(status$vector)," DOES NOT EXIST"));
-      test_results[[toString(status$vector)]]=c(price_vector[i]-price_vector[i-1]);
+    signals[i]=status$signal;
+    # populating test_results<START>
+    if (i>1){
+      test_results[i]=(status$vector);
+      observed_jump[i] = price_vector[i]-price_vector[i-1];
+      # populating test_results<END>
     }
-    else{
-      print(paste(toString(status$vector),"EXISTS"));
-      test_results[[toString(status$vector)]]=c(test_results[[toString(status$vector)]],price_vector[i]-price_vector[i-1])
-    }
-    print(paste("signal for ",date_vector[i], " was ", signals[i], " price today: ",price_vector[i]  ));
+    #print(paste("signal for ",date_vector[i], " was ", signals[i], " price today: ",price_vector[i]));
+    
   }
-  ## populating test_results<end>
-  # plotting test_results <start>
-  len_signals = length(names(test_results));
-  #             toString(summary(res)$coef[,"t value"][2]),
+  par(mfrow=c(2,1))
   
-  print(paste("total number of signs: ",len_signals))
-  par(mfrow=c(floor(len_signals/5)+1,5))
-  for (i in seq(len_signals)){
-    plot(test_results[[i]],xlab=
-           paste(names(test_results)[i],"(sd=",sd(test_results[[i]]),")")
-                 )
-    #abline(res)
-  }
-  # plotting test_results <end>
+  plot(date_vector,test_results)
+  plot(date_vector,observed_jump)
+  stop("FINISHED generating DATA")
   return(signals);
   
 }
